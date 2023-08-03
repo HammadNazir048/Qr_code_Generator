118f4cd6e44f5366fc713a424b4b7aeb418b0755
Muhammad Hammad Nazir <116563563+HammadNazir048@users.noreply.github.com>
2023-08-03 11:49:47 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+function genQr(){
+    var gapi="https://chart.googleapis.com/chart?cht=qr&chs=";
+    var myimg=document.getElementById("img");
+    var mytext=document.getElementById("qrtext").value;
+    var mysize=document.getElementById("size").value;
+
+    if(mytext !="" && mysize=="100"){
+     myimg.src=gapi+"100x100"+"&chl="+mytext;
+    }
+    else if(mytext!="" && mysize=="150"){
+     myimg.src=gapi+"100x100"+"&chl="+mytext;
+    }
+    else if(mytext!="" && mysize=="200"){
+        myimg.src=gapi+"200x200"+"&chl="+mytext;
+    }
+    else if(mytext!="" && mysize=="250"){
+     myimg.src=gapi+"250x250"+"&chl="+mytext;
+    }
+    else{
+        alert("Please Enter the text")
+    }
+}
